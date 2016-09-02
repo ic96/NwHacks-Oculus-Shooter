3f16b30ef091f2866f5897e8f486770a91987ba3
Ian <ian.chan96@hotmail.com>
2016-09-01 20:03:08 -0700

    Add files via upload

---
+++ script.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script : MonoBehaviour {
+
+	public GameObject[] enemies;
+	public int amount;
+	private Vector3 spawnPoint;
+	
+	// Update is called once per frame
+	void Update () {
+		amount = enemies.Length;
+		if (amount != 10) {
+			InvokeRepeating ("spawnEnemy", 1, 1f);
+		}
+	}
+
+	void spawnEnemy () {
+		spawnPoint.x = Random.Range (-20, 20);
+		spawnPoint.y = 1f;
+		spawnPoint.z = Random.Range (-20, 20);
+		Instantiate (enemies [UnityEngine.Random.Range (0, enemies.Length - 1)], spawnPoint, Quaternion.identity);
+		CancelInvoke ();
+	}
+}
---
+++ zombiewalk.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zombiewalk : MonoBehaviour {
+	public Transform player;
+	private float moveSpeed = 3f;
+	public GameObject target;
+
+	// Update is called once per frame
+	void Update () {
+		float move = moveSpeed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, player.position, move);
+		Vector3 n = target.transform.position - transform.position;
+		transform.rotation = Quaternion.LookRotation (n);
+	}
+}
